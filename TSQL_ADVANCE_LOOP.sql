2a667cd970fa2c22693ea44d0b993f91f254ac51
Raheleh Moallemi <87756714+RahelehMoa@users.noreply.github.com>
2026-06-09 17:24:03 +0330

    Add files via upload

---
+++ TSQL_ADVANCE_LOOP.sql
@@ -0,0 +1,40 @@
+--///////////////////////////////LOOP//////////////////////////////
+
+DECLARE @i int = 1;
+
+WHILE @i <= 10   --condition 
+BEGIN
+  PRINT ' i = ' +CAST(@i AS NVARCHAR)
+  SET @i = @i+1 
+END;
+
+--///////////////////////////////////////////////////////////////////////////////////////
+
+DECLARE @i int = 1;
+
+WHILE @i <= 10   --condition 
+BEGIN
+  PRINT ' i = ' +CAST(@i AS NVARCHAR)
+  IF @i = 5
+  BEGIN
+  PRINT 'END WHILE'
+  BREAK
+  END
+  SET @i = @i+1 
+
+END;
+--//////////////////////////////////////////////////////////////////////////
+
+-- یک متغییر تعریف می کنیم @i 
+DECLARE @i int = 0;
+
+--یک متغییر برای تعداد کل می شمارد  
+DECLARE @total int;
+
+-- یک متغییر برای نگهداری اسم های هر محصول
+DECLARE @productName NVARCHAR (100);
+
+-- تعداد کل محصولات را در متغییر توتال می ریزیم
+Select @total=COUNT(*) From SalesLT.Product
+
+-- از حلقه استفاده میکنیم که روی محصولات پیمایش انجام دهیم
