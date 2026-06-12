2584feb3acec7aacf1096f90b3d8415330087692
Raheleh Moallemi <87756714+RahelehMoa@users.noreply.github.com>
2026-06-12 16:58:23 +0330

    Add files via upload

--- TSQL_Beginner.sql
+++ TSQL_Beginner.sql
@@ -1,4 +1,4 @@
---//////////////////////////////SELELCT/////////////////////////////////////+
+--//////////////////////////////SELELCT/////////////////////////////////////
 SELECT * FROM SalesLT.[SalesLT.Customer];
 
 Select 20
@@ -63,6 +63,12 @@ CustomerAge Int,
 CreateDate DateTime
 )
 
+Create Table CustometTable(
+Id Int IDENTITY(1,1)  Primary Key,
+FIRSTNAME NVARCHAR(50) NOT NULL,
+LASTName NVARCHAR(200) NOT NULL,
+Age Int,
+)
 -- Primary Key is unic and dont repeat.
 
 --//////////////////////Replace Tabale/////////////////////////////
@@ -82,3 +88,108 @@ from SalesLT.Product;
 select * from ProductTest;
 
 --///////////////////////////INSERT INTO/////////////////////////////////
+
+select * from ProductOrder;
+
+Select * from ProductOrder1;       --with Primary Key
+
+INSERT INTO ProductOrder(Id,ProuctName,CustomerName,CustomerAge,CreateDate)
+VALUES(1,'LB','luca G',36,'6-12-2026')
+
+select * from ProductOrder;
+
+INSERT INTO ProductOrder(Id,ProuctName,CustomerName,CustomerAge,CreateDate)
+VALUES(1,'LC','luigi C',32,'6-12-2026'),
+(1,'DL','Daniel L',30,'6-12-2026'),
+(2,'MM','Monica M','', '6-12-2026'),
+(3,'SG','Sara G',24,'6-12-2026');
+
+INSERT INTO ProductOrder(Id,ProuctName,CustomerName)
+VALUES(1,'RM','Raheleh M');
+
+select * from ProductOrder;
+
+-- delete columns but put All recordes
+INSERT INTO ProductOrder    
+VALUES(4,'LG','Ludivica G', 32 , '6-12-2026');
+
+select * from ProductOrder;
+
+-- in Table ProductOrder1 be carful Ids are unic and dont repepat Id. 
+-- id = Primary Key.
+INSERT INTO ProductOrder1(Id,ProuctName,CustomerName,CustomerAge,CreateDate)
+VALUES(1,'LB','luca G',36,'6-12-2026'),
+(2,'DL','Daniel L',30,'6-12-2026'),
+(3,'MM','Monica M','', '6-12-2026'),
+(4,'SG','Sara G',24,'6-12-2026');
+
+select * from ProductOrder1;
+
+INSERT INTO ProductOrder1(Id,ProuctName,CustomerName)
+Select CustomerID,LastName,FirstName From SalesLT.[SalesLT.Customer]
+Where CustomerID = 20;
+
+select * from ProductOrder1;
+
+INSERT INTO ProductOrder(Id,ProuctName,CustomerName)
+Select CustomerID,LastName,FirstName From SalesLT.[SalesLT.Customer]
+Where CustomerID < 10;
+
+select * from ProductOrder;
+
+--INSERT INTO CustometTable (Id,FIRSTNAME,LASTNAME,Age) 
+--VALUES (1,'luca','G',36);   --ERROR becuse that is a Id uto generate = IDENTITY(1,1)
+
+INSERT INTO CustometTable (FIRSTNAME,LASTNAME,Age) 
+VALUES ('luca','G',36)
+
+INSERT INTO CustometTable (FIRSTNAME,LASTNAME,Age) 
+VALUES ('Sare','L',30),
+('Monica','M',32);
+
+select * from CustometTable;
+
+-- In Table "CustometTable" ids are unic and gennerate utomaticlly.
+-- we dont need used in insert to statemment.
+
+--/////////////////////////////////DELETE//////////////////////////////
+-- DELETE DATA OR RECORDES
+
+Delete From ProductOrder
+Where Id = 1;  
+
+-- we have 10 values that are deleted.
+
+select * from ProductOrder;
+
+--Delete From ProductOrder 
+
+-- Above statement will be deleted all recordes.
+-- Cash Data is not remove. 
+-- If after delete all recordes we insert into another values it will be 
+-- next number(Ex: Id=5) for Id that Id dont start from 1.
+
+--///////////////////////////////TRUNCATE/////////////////////////////
+
+Truncate Table ProductOrder1
+
+select * from ProductOrder1
+
+INSERT INTO ProductOrder1(Id,ProuctName,CustomerName,CustomerAge,CreateDate)
+VALUES(1,'LB','luca G',36,'6-12-2026');
+
+-- Id will be start from 1 but in delete Id start from next number for Ex:5.
+
+--//////////////////////////////DROP/////////////////////////////////
+-- Delelte Whole Table and we can not see Table in left side in Tables of Database
+
+Drop Table ProductNew
+
+
+-- DELLETE FROM is Used for Delete Some of DATA and Records with Condition WHERE.
+-- TRUNCATE TABLE is Used for Delete ALL Recordes and Cash that start from Id = 1.
+-- DROPE TABLE is used for Delete whole table from Database.
+
+-- Note: IN delete with DELLETE AND TRUNCATE after delete we can insert into values
+-- in the table but after DROPE we can not insert values becuse table is deleted 
+-- from Database.
