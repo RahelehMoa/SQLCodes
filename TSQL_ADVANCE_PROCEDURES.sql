70682983ad51e361e651fa731caa6c8c3ae61e12
Raheleh Moallemi <87756714+RahelehMoa@users.noreply.github.com>
2026-06-23 11:18:57 +0330

    Add files via upload

--- TSQL_ADVANCE_PROCEDURES.sql
+++ TSQL_ADVANCE_PROCEDURES.sql
@@ -1,7 +1,10 @@
---///////////////////////////PROCEDURES////////////////////////////////////
+ --///////////////////////////PROCEDURES////////////////////////////////////
 
 Select * From SalesLT.Product
-
+--create Procedure NAME-PROCEDURE
+--AS
+--BEGIN
+--END;
 --///////////////////////////Procedure Basic//////////////////////////////
 Create Procedure ProductStorewithBlackColor
 AS
@@ -10,10 +13,10 @@ BEGIN
    WHERE Color='Black'
 END;
 
-EXEC ProductStorewithBlackColor
+EXEC ProductStorewithBlackColor      -- RUN PROCEDURE
 
 --فرقش با ویو این است که مقدار متغییر میدهیم و هر مقداری که خواستیم در متغییر قرار میدهیم
-
+-- Define A Variable Color of NVARCHAR then insted of 'Black' we put Variable.
 Create Procedure ProductStorewithColor
        @Color NVARCHAR(30)
 AS
@@ -26,7 +29,7 @@ EXEC ProductStorewithColor @Color = 'Red'
 EXEC ProductStorewithColor @Color = 'White'
 EXEC ProductStorewithColor @Color = 'Blue'
 
-
+--////////////////////////////////////////////////////
 Alter Procedure ProductStorewithColorAndWeight
        @Color NVARCHAR(30),
        @Weight float 
@@ -38,6 +41,8 @@ END;
 
 EXEC ProductStorewithColorAndWeight @Color = 'Red',@Weight = 1000
 
+--Be carful: in define Variable we put < but in define value , EXEC, we put = 
+
 --/////////////////////////Procedure Advance////////////////////////////////
 -- ابتدا وروردی های جدول که @ و نام ستون ها است را وارد میکنیم
 --سپس خروجی را به صورت output  با متغییری تعریف میکنیم.
@@ -48,7 +53,7 @@ Alter Procedure AddUsers     ---Create or Alter
 @Age int,
 @Birthday datetime,
 @JobID  nvarchar(100),
-@Result nvarchar(50) OUTPUT
+@Result nvarchar(50) OUTPUT     --OUtput = متغییر خروجی
 As
 BEGIN
 
