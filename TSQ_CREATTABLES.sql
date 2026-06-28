88b48644b6caa359393ad55ce3f354c789856428
Raheleh Moallemi <87756714+RahelehMoa@users.noreply.github.com>
2026-06-28 13:17:46 +0330

    Add files via upload

--- TSQ_CREATTABLES.sql
+++ TSQ_CREATTABLES.sql
@@ -9,3 +9,16 @@ JobID  nvarchar(100));
 INSERT INTO USERS VALUES (1,'JAN lEE','JAN.LEE@GMAIL.COM',34,19/5/1997,'1l');
 
 SELECT * fROM USERS
+
+--/////////////////////////USER JOBS//////////////////////////
+
+create table USERS_JOBS 
+(JobID nvarchar(100) Primary Key, 
+JobName nvarchar(50)
+);
+
+INSERT INTO USERS_JOBS  VALUES ('22G','Teacher'), 
+                               ('25G','Photografer'),
+                               ('30D','Cocker');
+
+SELECT * fROM USERS_JOBS 
